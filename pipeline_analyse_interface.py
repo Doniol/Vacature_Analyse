b73eab042523c8a0b7c3c7106053e3c18f22a751
Doniol <43216456+Doniol@users.noreply.github.com>
2020-10-21 15:14:33 +0200

    Pipeline analyse -> interface

--- pipeline_analyse_interface.py
+++ pipeline_analyse_interface.py
@@ -1,30 +1,72 @@
 from pipeline_base import database_connection
 from typing import Union, List, Dict, Tuple, Any
+import datetime
 
 class pipeline_analyse_to_db(database_connection):
     def __init__(self, host: str, port: str, database_name: str, user: str, password: str) -> None:
         database_connection.__init__(self, host, port, database_name, user, password)
     
-    def add_new_entry(self, word: str, institute_id: int, count: int, institute: str) -> None:
-        self.enter_command("INSERT INTO {0} VALUES (\'{1}\', {2}, {3})".format(institute, word, institute_id, count))
+    def add_new_entry(self, word: str, count: int, institute: str) -> None:
+        current_time = datetime.datetime.now()
+        date_id = self.add_unique_entry("dates_", "date", "date_id", current_time.strftime("%Y") +"-" + current_time.strftime("%M"))
+        word_id = self.add_unique_entry("words_", "word", "word_id", word)
+        institute_id = self.add_unique_entry("institutes_", "institute", "institute_id", institute)
+        self.enter_command("INSERT INTO entries_ (word, word_count, date, institute) VALUES ({0}, {1}, {2}, {3})".format(word_id, count, date_id, institute_id))
     
-    def add_dict(self, words: List[str], institute: str) -> None:
+    def add_dict(self, words, institute: str) -> None:
         for key in words:
-            self.add_new_entry(key, words[key][1], words[key][0], institute)
+            self.add_new_entry(key, words[key], institute)
+
+    def add_unique_entry(self, table_name, attribute_name, value):
+        if not self.fetch_command("SELECT * FROM {0} WHERE {1} = {2}".format(table_name, attribute_name, value)):
+            self.enter_command("INSERT INTO {0} ({1}) VALUES ({2})".format(table_name, attribute_name, value))
+        return self.fetch_command("SELECT {0}_id FROM {1} WHERE {0} = {2}".format(attribute_name, table_name, value))
 
 
 class pipeline_db_to_interface(database_connection):
     def __init__(self, host: str, port: str, database_name: str, user: str, password: str) -> None:
         database_connection.__init__(self, host, port, database_name, user, password)
 
-    def get_by_institute(self, institute: str) -> List[Tuple[str, int, int]]:
-        return self.fetch_command("SELECT * FROM {}".format(institute))
+    def get_id_entity(self, table_name, attribute_name, value):
+        return self.fetch_command("SELECT {0}_id FROM {1} WHERE {0} = {2}".format(attribute_name, table_name, value))
+
+    def get_entries(self, date=None, word=None, institute=None):
+        query = ""
+        if date:
+            query += self.add_to_query(query, "date", self.get_id_entity("dates_", "date", date))
+        if word:
+            query += self.add_to_query(query, "word", self.get_id_entity("words_", "word", word))
+        if institute:
+            query += self.add_to_query(query, "institute", self.get_id_entity("institutes_", "institute", institute))
+        return self.fetch_command("SELECT * FROM entries_" + query) ## List[Tuple[entry_id, word_id, word_count, date_id, institute_id]]
     
-    def get_all_entries(self) -> List[Tuple[List[str], List[Tuple[str, int, int]]]]:
-        entries = []
-        for institute in self.fetch_command("select table_name from innodb.INFORMATION_SCHEMA.TABLES where TABLE_TYPE = 'BASE TABLE' and not table_name like 'pg_%' and not table_name like 'sql_%'"):
-            entries.append([institute, self.get_by_institute(institute[0])])
-        return entries
+    def add_to_query(self, query, attribute_name, attribute_id):
+        if len(query) > 0:
+            query += " AND "
+        else:
+            query += " WHERE "
+        return query + attribute_name + " = " + attribute_id
+
+    def get_by_time_period(self, start_date, end_date, word=None, institute=None):
+        start_date_id = self.get_id_entity("dates_", "date", start_date)
+        end_date_id = self.get_id_entity("dates_", "date", start_date)
+        data = {}
+        for date_id in range(end_date_id, start_date_id + 1):
+            actual_date = self.fetch_command("SELECT date FROM dates_ WHERE date_id = {0}".format(date_id))
+            data[actual_date] = self.get_entries(date=actual_date, word=word, institute=institute)
+        return data
+        
+    def get_lookup_table(self, table_name):
+        return self.fetch_command("SELECT * FROM {0}".format(table_name))
+
+    
+    def get_dict(self, data_entries):
+        dates_all_info = self.get_lookup_table("")
+
+
+
+
+
 
     def create_dict(self, institute: str) -> Union[Dict[str, int], Dict[str, Dict[str, int]]]:
         if institute == "*":
@@ -32,10 +74,14 @@ class pipeline_db_to_interface(database_connection):
         else:
             return self.get_dict(self.get_by_institute(institute))
 
-    def get_dict(self, data_entries: List[Tuple[str, int, int]]) -> Dict[str, int]:
+    def get_dict(self, data_entries: List[Tuple[int, int, int]]) -> Dict[str, int]:
+        dates_all_info = self.get_lookup_table("dates_") ## (word, word_id)
+        words_all_info = self.get_lookup_table("words_") ## (date, date_id)
+
         entry_dict = {}
+
         for data_entry in data_entries:
-            entry_dict[data_entry[0]] = data_entry[2]
+            entry_dict[words_all_info[data_entry[0]][1]] += data_entry[1]
         return entry_dict
     
     def get_multi_dict(self, dataset: List[Tuple[List[str], List[Tuple[str, int, int]]]]) -> Dict[str, Dict[str, int]]:
@@ -44,5 +90,5 @@ class pipeline_db_to_interface(database_connection):
             entry_dict = {}
             for data_entry in data_entries[1]:
                 entry_dict[data_entry[0]] = data_entry[2]
-            multi_dict[data_entries[0][0]] = entry_dict
+            multi_dict[data_entries[0][0]] += entry_dict
         return multi_dict
--- test.py
+++ test.py
@@ -1,24 +1,19 @@
-from pipeline_verzamelaar_analyse import pipeline_db_to_analyse
-from pipeline_analyse_interface import pipeline_analyse_to_db, pipeline_db_to_interface
+import plotly.graph_objects as go
+import numpy as np
+from pipeline_analyse_interface import pipeline_db_to_interface as pipeline
 
 
-test = pipeline_db_to_analyse(
-    host="weert.lucimmerzeel.nl",
-    port="5432",
-    database_name="pocdb",
-    user="pocuser",
-    password="pocuser")
+def main():
+    host = "weert.lucimmerzeel.nl"
+    port = "5432"
+    database_name = "innodb"
+    user = "innouser"
+    password = "innouser"
+    db = pipeline(host, port, database_name, user, password)
+    data = db.fetch_command("SELECT * FROM institute_ict")
+    for thing in data:
+        print(thing[0].strip(), thing[1])
 
-test = pipeline_analyse_to_db(
-    host="weert.lucimmerzeel.nl",
-    port="5432",
-    database_name="pocdb",
-    user="pocuser",
-    password="pocuser")
 
-test = pipeline_db_to_interface(
-    host="weert.lucimmerzeel.nl",
-    port="5432",
-    database_name="pocdb",
-    user="pocuser",
-    password="pocuser")
+if __name__ == "__main__":
+    main()
