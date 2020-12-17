04359aee94555d2ab68b4c00d1173f610ac72f12
Doniol <danielvaneijk-bos@live.nl>
2020-12-17 12:22:35 +0100

    Trained AI?

--- AI/run_all_AI.py
+++ AI/run_all_AI.py
@@ -5,8 +5,8 @@ import numpy as np
 from keras.backend import clear_session
 
 
-def train_AI(AI, file):
-    AI.train_AI(1, file)
+def train_AI(AI, out_file, in_file=None):
+    AI.train_AI(1, out_file, weights_input=in_file)
 
 
 def run_AI(AI, file):
@@ -26,28 +26,28 @@ def main():
     AI_S_NP = AI_Seq2SeqNoPadding(datasets, 4)
 
     # Train AI
-    # print("AI_LSTM_Padding_1")
-    # AI_L_P.train_AI(1, "AI\obj\AI_LSTM_Padding_1", "AI\obj\AI_LSTM_Padding_1")
-    # print("AI_LSTM_Padding_bidirectional_1")
-    # train_AI(AI_L_P_b, "AI\obj\AI_LSTM_Padding_bidirectional_1", "AI\obj\AI_LSTM_Padding_bidirectional_1")
+    print("AI_LSTM_Padding_1")
+    AI_L_P.train_AI(1, "AI\obj\AI_LSTM_Padding_1", "AI\obj\AI_LSTM_Padding_1")
+    print("AI_LSTM_Padding_bidirectional_1")
+    train_AI(AI_L_P_b, "AI\obj\AI_LSTM_Padding_bidirectional_1", "AI\obj\AI_LSTM_Padding_bidirectional_1")
 
     #TODO: Cant run the following 2 in the same function?!?!?!??! (The Seq2Seq's that is)
-    # print("AI_Seq2SeqPadding_1")
-    # train_AI(AI_S_P, "AI\obj\AI_Seq2SeqPadding_1", "AI\obj\AI_Seq2SeqPadding_1")
-    # print("AI_Seq2SeqNoPadding_1")
-    # train_AI(AI_S_NP, "AI\obj\AI_Seq2SeqNoPadding_1", "AI\obj\AI_Seq2SeqNoPadding_1")
+    print("AI_Seq2SeqPadding_1")
+    train_AI(AI_S_P, "AI\obj\AI_Seq2SeqPadding_1", "AI\obj\AI_Seq2SeqPadding_1")
+    print("AI_Seq2SeqNoPadding_1")
+    train_AI(AI_S_NP, "AI\obj\AI_Seq2SeqNoPadding_1", "AI\obj\AI_Seq2SeqNoPadding_1")
 
     # Run AI
-    print("AI_LSTM_Padding_1")
-    run_AI(AI_L_P, "AI\obj\AI_LSTM_Padding_1")
-    print("AI_LSTM_Padding_bidirectional_1")
-    run_AI(AI_L_P_b, "AI\obj\AI_LSTM_Padding_bidirectional_1")
+    # print("AI_LSTM_Padding_1")
+    # run_AI(AI_L_P, "AI\obj\AI_LSTM_Padding_1")
+    # print("AI_LSTM_Padding_bidirectional_1")
+    # run_AI(AI_L_P_b, "AI\obj\AI_LSTM_Padding_bidirectional_1")
     
     # Create exactly the same AI anew in the function call, if i just reference to the existing AI it starts fucking up and giving weirdass errors
-    print("AI_Seq2SeqPadding_1")
-    run_AI(AI_Seq2SeqNoPadding(datasets, 4), "AI\obj\AI_Seq2SeqPadding_1")
-    print("AI_Seq2SeqNoPadding_1")
-    run_AI(AI_Seq2SeqPadding(datasets, 1000, 4), "AI\obj\AI_Seq2SeqNoPadding_1")
+    # print("AI_Seq2SeqPadding_1")
+    # run_AI(AI_Seq2SeqNoPadding(datasets, 4), "AI\obj\AI_Seq2SeqPadding_1")
+    # print("AI_Seq2SeqNoPadding_1")
+    # run_AI(AI_Seq2SeqPadding(datasets, 1000, 4), "AI\obj\AI_Seq2SeqNoPadding_1")
 
 
 main()
